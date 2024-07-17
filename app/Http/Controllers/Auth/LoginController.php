44267c78138f7dd0cab2bf94de968a40b68dd358
vishvas_ashapurasoftech <vishwas.s@ashapurasoftech.com>
2024-07-17 19:07:18 +0530

    Implement Login with Google and Github

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+        $this->middleware('auth')->only('logout');
+    }
+
+
+     // Google login
+     public function redirectToGoogle()
+     {
+         return Socialite::driver('google')->redirect();
+     }
+
+      // Google callback
+    public function handleGoogleCallback()
+    {
+        // $user = Socialite::driver('google')->user();
+        $user = Socialite::driver('google')->stateless()->user();
+
+        $this->_registerOrLoginUser($user);
+
+        // Return home after login
+        return redirect()->route('home');
+    }
+
+    //  // Facebook login
+    //  public function redirectToFacebook()
+    //  {
+    //      return Socialite::driver('facebook')->redirect();
+    //  }
+
+    //  // Facebook callback
+    //  public function handleFacebookCallback()
+    //  {
+    //      $user = Socialite::driver('facebook')->user();
+
+    //      $this->_registerOrLoginUser($user);
+
+    //      // Return home after login
+    //      return redirect()->route('home');
+    //  }
+
+     // Github login
+     public function redirectToGithub()
+     {
+         return Socialite::driver('github')->redirect();
+     }
+
+     // Github callback
+     public function handleGithubCallback()
+     {
+        //  $user = Socialite::driver('github')->user();
+         $user = Socialite::driver('github')->stateless()->user();
+
+         $this->_registerOrLoginUser($user);
+
+         // Return home after login
+         return redirect()->route('home');
+     }
+
+     protected function _registerOrLoginUser($data)
+    {
+
+        // dd($data);
+        $user = User::where('email', '=', $data->email)->first();
+        if (!$user) {
+            $user = new User();
+            $user->name = $user->name ?? "-";;
+            $user->email = $data->email;
+            $user->provider_id = $data->id;
+            $user->avatar = $data->avatar;
+            $user->save();
+        }
+        Auth::login($user);
+    }
+}
